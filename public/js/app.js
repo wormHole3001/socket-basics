822d3a2d7a568dd633781979c22bc83b2d28dfea
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2020-05-13 23:50:27 -0500

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 
 socket.on('connect', function () {
     console.log('Connected to socket.io server.');
@@ -7,10 +9,12 @@ socket.on('connect', function () {
 // Front end listen for message (callback)
 socket.on('message', function (message) {
     var momentTimeStamp = moment.utc(message.timestamp);
+    var $message = jQuery('.messages');
     console.log('New message:');
     console.log(message.text);
 
-    jQuery('.messages').append('<p><strong>' + momentTimeStamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
+    $message.append('<p><strong>' + message.name + ' ' + momentTimeStamp.local().format('h:mm a') + '</strong></p>')
+    $message.append('<p>' + message.text + '</p>')
 });
 
 // Handle submitting new messages
@@ -19,6 +23,7 @@ $form.on('submit', function (event) {
     event.preventDefault(); // Don't submit by refreshing the entire page
     var $message = $form.find('input[name=message]');
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     $message.val(''); // Used to clear text box after sending message
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.on('connection', function (socket) {
 
     // Greeting for the user once they are connected via socket.
     socket.emit('message', {
+        name: 'System',
         text: 'Welcome to the chat application',
         timestamp: moment().valueOf()
     });
