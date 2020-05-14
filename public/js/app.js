59909ecf723af8f5cd1ae301ef8a02845c76408e
Fernando Gonzalez <fgonzalez19@islander.tamucc.edu>
2020-05-13 22:25:18 -0500

    Added message event
    Added custom message event to back and front end

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to socket.io server.');
 });
+
+// Front end listen for message
+socket.on('message', function (message) {
+    console.log('New message:');
+    console.log(message.text);
+});
